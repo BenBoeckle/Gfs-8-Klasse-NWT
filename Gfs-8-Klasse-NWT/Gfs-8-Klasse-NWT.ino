ae70bc0d6451c3f282686fd58adf64b48928b4d8
Ben <93058166+BenBoeckle@users.noreply.github.com>
2022-11-22 17:31:52 +0100

    Add files via upload

--- Gfs-8-Klasse-NWT/Gfs-8-Klasse-NWT.ino
+++ Gfs-8-Klasse-NWT/Gfs-8-Klasse-NWT.ino
@@ -1,78 +1,156 @@
 
-#define TASTE1 2
-#define TASTE2 3
-#define DREHDINGS A0
-#define ECHO 7
-#define TRIGGER 6
-#define LED_LENKUNG 13
-#define PWM 11
-#define MOTOR1 10
-#define MOTOR2 9
+#define TASTE_START_AUTOMATISCH 9
 
 
-void setup() {
+
+//Abstandssensor
+#define ECHO 11
+#define TRIGGER 10
+#define OUTPUT_ABSTAND 8
+
+#define PWM_RICHTUNG 3
+#define MOTOR_RICHTUNG1 7
+#define MOTOR_RICHTUNG2 6
+
+#define PWM_LENKUNG 4
+#define MOTOR_LENKUNG1 2
+#define MOTOR_LENKUNG2 5
+
+
+
+void setup() 
+{
   // put your setup code here, to run once:
-  pinMode(TASTE1, INPUT_PULLUP);
-  pinMode(TASTE2, INPUT_PULLUP);
+  pinMode(TASTE_START_AUTOMATISCH, INPUT_PULLUP);
+  
+  // Abstandssensor
   pinMode(TRIGGER, OUTPUT);
   pinMode(ECHO, INPUT);
-  pinMode(LED_LENKUNG, OUTPUT);
-  pinMode(MOTOR1, OUTPUT);
-  pinMode(MOTOR2, OUTPUT);
-  pinMode(PWM, OUTPUT);
-  Serial.begin(115200);
- 
+  pinMode(OUTPUT_ABSTAND, OUTPUT);
+
+  // Motor vorwärts/rückwärts
+  pinMode(PWM_RICHTUNG, OUTPUT);
+  pinMode(MOTOR_RICHTUNG1, OUTPUT);
+  pinMode(MOTOR_RICHTUNG2, OUTPUT);
+
+  // Motor rechts/links
+  pinMode(PWM_LENKUNG, OUTPUT);
+  pinMode(MOTOR_LENKUNG1, OUTPUT);
+  pinMode(MOTOR_LENKUNG2, OUTPUT);
+  
+  Serial.begin(115200); 
 }
 
-void loop() {
+void loop() 
+{
+  unsigned long StopLenkungZeit = 0;
   // put your main code here, to run repeatedly:
-  int Taste1 = digitalRead(TASTE1);
-  int Taste2 = digitalRead(TASTE2);
-  Serial.print("Taste1: ");
-  Serial.print(Taste1);
-  Serial.print(" / Taste2: ");
-  Serial.print(Taste2); 
-  
-  // Abstands Messung
-  digitalWrite(TRIGGER, LOW);
-  delayMicroseconds(2);  
-  digitalWrite(TRIGGER, HIGH);
-  delayMicroseconds(10);
-  digitalWrite(TRIGGER, LOW);
-  long dauer = pulseIn(ECHO, HIGH);
-  long entfernung = (dauer/2) * 0.03432; 
-  if (entfernung  <= 16)
+  int Taste1 = digitalRead(TASTE_START_AUTOMATISCH);
+ 
+  // Warte bis Start gedrückt wird
+  while (Taste1 == HIGH )
   {
-    digitalWrite(LED_LENKUNG, HIGH);
   }
-  else 
-  {
-    digitalWrite(LED_LENKUNG, LOW);
-  }
-  Serial.print(" / Entfernung: ");
-  Serial.print(entfernung);
-  Serial.print(" cm");
+  delay(1000);
 
-  int Drehdings = analogRead(DREHDINGS);
-  Serial.print(" / Drehdings: ");
-  Serial.println(Drehdings);
-  analogWrite(PWM, Drehdings/4);
+  digitalWrite(OUTPUT_ABSTAND, LOW);
+
+  // Lenkung auf 0
+  analogWrite(PWM_LENKUNG, 0);
+  digitalWrite(MOTOR_LENKUNG1, LOW);
+  digitalWrite(MOTOR_LENKUNG2, LOW);
+
+
+  int Geschwindigkeit = 200; //analogRead(GESCHWINDIGKEIT) / 4;
+  analogWrite(PWM_RICHTUNG, Geschwindigkeit);
+  digitalWrite(MOTOR_RICHTUNG1, HIGH);
+  digitalWrite(MOTOR_RICHTUNG2, LOW);
+
+  delay(1000);
+  analogWrite(PWM_RICHTUNG, Geschwindigkeit * 0.8);
+
+
+  // Solange Taste nicht gedrückt wird
+  while (Taste1 == HIGH)
+  {
+    // Abstands Messung
+    digitalWrite(TRIGGER, LOW);
+    delayMicroseconds(2);  
+    digitalWrite(TRIGGER, HIGH);
+    delayMicroseconds(10);
+    digitalWrite(TRIGGER, LOW);
+    long dauer = pulseIn(ECHO, HIGH);
+    long entfernung = (dauer/2) * 0.03432; 
+    if (entfernung  < 10)
+    {
+      digitalWrite(OUTPUT_ABSTAND, HIGH);
+
+      // Zu nahe am Hindernis, anhalten
+      analogWrite(PWM_RICHTUNG, 0);
+      digitalWrite(MOTOR_RICHTUNG1, LOW);
+      digitalWrite(MOTOR_RICHTUNG2, LOW);
+    }
+    else if (entfernung  < 70)
+    {
+      digitalWrite(OUTPUT_ABSTAND, HIGH);
+      analogWrite(PWM_RICHTUNG, Geschwindigkeit * 0.85);
+
+      // Starte mit Lenkung
+      analogWrite(PWM_LENKUNG, 220);
+      digitalWrite(MOTOR_LENKUNG1, HIGH);
+      digitalWrite(MOTOR_LENKUNG2, LOW);
+
+      StopLenkungZeit = micros();
+    }
+    else 
+    {
+      digitalWrite(OUTPUT_ABSTAND, LOW);
+
+      if ( micros() - StopLenkungZeit > 1200 * 1000) 
+      {
+        // Stoppe Lenkung
+        analogWrite(PWM_RICHTUNG, Geschwindigkeit * 0.8);
+
+        analogWrite(PWM_LENKUNG, 0);
+        digitalWrite(MOTOR_LENKUNG1, LOW);
+        digitalWrite(MOTOR_LENKUNG2, LOW);        
+      }
+    }
+  }
+
+  // Richtung auf stopp
+  analogWrite(PWM_RICHTUNG, 0);
+  digitalWrite(MOTOR_RICHTUNG1, LOW);
+  digitalWrite(MOTOR_RICHTUNG2, LOW);
+
+
+  // Lenkung auf stopp
+  analogWrite(PWM_LENKUNG, 0);
+  digitalWrite(MOTOR_LENKUNG1, LOW);
+  digitalWrite(MOTOR_LENKUNG2, LOW);
+
+
+/*
+  //digitalWrite(MOTOR_RICHTUNG1, Taste1 == LOW ? HIGH : LOW);
+  digitalWrite(MOTOR_RICHTUNG2, Taste2 == LOW ? HIGH : LOW);
+
   if (Taste1 == LOW)
   {  
-    digitalWrite(MOTOR1, HIGH);
-    digitalWrite(MOTOR2, LOW);
+    tone(8, 50);
   }    
-  else if (Taste2 == LOW)
-  {
-    digitalWrite(MOTOR1, LOW);
-    digitalWrite(MOTOR2, HIGH);
-  }
   else
   {
-    digitalWrite(MOTOR1, LOW);
-    digitalWrite(MOTOR2, LOW);
+    noTone(8);
   }
-  
-  
+  */
+
+
+
+
+
+
+
+
+
   delay(500);
 }
